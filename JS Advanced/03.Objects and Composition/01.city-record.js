e025cc7b689077308f52b0f2959cd86187b61096
Ангел Спасов <aspasov_@abv.bg>
2021-11-25 23:51:18 +0200

    Add files via upload

---
+++ JS Advanced/03.Objects and Composition/01.city-record.js
@@ -0,0 +1,8 @@
+function solve(name, population, treasury) {
+    let cityInfo = {};
+    cityInfo.name = name,
+    cityInfo.population = population,
+    cityInfo.treasury = treasury
+    return cityInfo;
+}
+solve("as", 123, 234);
