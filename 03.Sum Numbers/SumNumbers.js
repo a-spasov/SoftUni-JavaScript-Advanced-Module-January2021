fb0f280908a47cee9e1f6c34b962ed11c366132c
Ангел Спасов <aspasov_@abv.bg>
2022-01-20 23:34:17 +0200

    Delete 03.Sum Numbers directory

--- 03.Sum Numbers/SumNumbers.js
+++
@@ -1,6 +0,0 @@
-function calc() {
-    let firstNumber = document.querySelector('#num1').value;
-    let secondNumber = document.querySelector('#num2').value;
-    let result = Number(firstNumber) + Number(secondNumber);
-    document.querySelector('#sum').value = result;
-}
