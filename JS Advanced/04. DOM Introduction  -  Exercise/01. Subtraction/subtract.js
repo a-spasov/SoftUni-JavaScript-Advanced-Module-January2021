06226008acdee8b0052cc59c91ee1990403a6bff
Ангел Спасов <aspasov_@abv.bg>
2022-01-27 23:51:04 +0200

    Delete subtract.js

--- JS Advanced/04. DOM Introduction  -  Exercise/01. Subtraction/subtract.js
+++
@@ -1,7 +0,0 @@
-function subtract() {
-    let num1 = document.querySelector('#firstNumber').value;
-    let num2 = document.querySelector('#secondNumber').value;
-    let result = num1 - num2;
-    document.querySelector('#result').textContent = result;
-    console.log('TODO:...');
-}
