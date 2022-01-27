beb014c601f718cc12f357de70ac40a87d7a1689
Ангел Спасов <aspasov_@abv.bg>
2022-01-27 23:50:05 +0200

    Add files via upload

---
+++ JS Advanced/04. DOM Introduction  -  Exercise/01. Subtraction/01. Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    let num1 = document.querySelector('#firstNumber').value;
+    let num2 = document.querySelector('#secondNumber').value;
+    let result = num1 - num2;
+    document.querySelector('#result').textContent = result;
+    console.log('TODO:...');
+}
